8c6d0896bf4263433dcf3489a7cb05c95c828a14
maximizzar <60427093+maximizzar@users.noreply.github.com>
2023-08-15 01:22:03 +0200

    get_address_from_config and get_address_from_web fully functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-
 use curl::easy::Easy;
+use regex::Regex;
 
 use std::env;
 use std::env::args;
@@ -26,14 +26,23 @@ fn main() {
 }
 
 fn get_address_from_config(config: &String, hostname: &String) -> String {
+    let mut host_record = String::new();
     for line in config.lines() {
         if line.contains(hostname) && line.contains("AAAA") {
-            return line.to_string();
+            host_record = line.to_string();
         }
     }
-    return "".to_string();
+    let mut prefix = String::new();
+    {
+        let re: Regex = Regex::new(r#"(\w+\s+)(IN\s+)(AAAA\s+)"#).unwrap();
+        let Some(prefix_in_config) = re.captures(host_record.as_ref()) else {
+            println!("no match!");
+            return "".to_string();
+        };
+        let _ = &prefix_in_config[0].clone_into(&mut prefix);
+    }
+    return host_record.strip_prefix(&prefix).unwrap().to_string();
 }
-
 fn get_address_from_web() -> String {
     // First write everything into a `Vec<u8>`
     let mut data = Vec::new();
@@ -49,7 +58,23 @@ fn get_address_from_web() -> String {
     }
 
     // Convert it to `String`
-    let body: String = String::from_utf8(data).expect("body is not valid UTF8!");
+    let mut body: String = String::from_utf8(data).expect("body is not valid UTF8!");
+
+    let prefix: String = r#"{"ip":""#.to_string();
+    let mut suffix: String = String::new();
+    {
+        let re: Regex = Regex::new(r#"(","host\S+)"#).unwrap();
+        let Some(suffix_in_body) = re.captures(body.as_ref()) else {
+            println!("no match!");
+            return "".to_string();
+        };
+        let _ = &suffix_in_body[0].clone_into(&mut suffix);
+    }
+
+    //Remove prefix and suffix from returned body
+    body = body.strip_prefix(&prefix).unwrap().to_string();
+    body = body.strip_suffix(&suffix).unwrap().to_string();
+
     return body;
 }
 
