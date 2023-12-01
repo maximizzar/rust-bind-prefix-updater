3b890603307006492cc1af083b7115e31332ce32
maximizzar <60427093+maximizzar@users.noreply.github.com>
2023-12-01 08:12:20 +0100

    refactor: removed get_address_from_web, use MyIp::get_current_netmask from now on

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod web_ip_checker;
+
 use regex::Regex;
 use ipnet::*;
 
 use std::io::{Read, Write};
 use std::str::FromStr;
+use crate::web_ip_checker::MyIp;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -14,12 +17,12 @@ fn main() {
     // put cli arguments into variables
     let hostname: String = args.get(1).unwrap().to_string();
     let config_path: &String = args.get(2).unwrap();
-    let netmask:u8 = args.get(3).unwrap().parse().unwrap();
+    let prefix_size: u8 = args.get(3).unwrap().parse().unwrap();
 
     //load data into variables
     let config: String = read_config(config_path);
-    let address_from_config = get_address_from_config(&config, &hostname, &netmask);
-    let address_from_web = get_address_from_web(&netmask);
+    let address_from_config = get_address_from_config(&config, &hostname, &prefix_size);
+    let address_from_web = MyIp::get_current_netmask(&prefix_size).unwrap();
 
     compare_prefixes(&address_from_config, &address_from_web);
     println!("prefixes differ from each other. Start to update config now!\n");
@@ -58,42 +61,6 @@ fn get_address_from_config(config: &String, hostname: &String, netmask: &u8) -> 
 
     return Ipv6Net::from_str(host_record.as_str()).unwrap();
 }
-fn get_address_from_web(netmask: &u8) -> Ipv6Net {
-    // First write everything into a `Vec<u8>`
-    let mut data = Vec::new();
-    let mut handle = curl::easy::Easy::new();
-    handle.url("https://6.myip.is/").unwrap();
-    {
-        let mut transfer = handle.transfer();
-        transfer.write_function(|new_data| {
-            data.extend_from_slice(new_data);
-            Ok(new_data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
-
-    // Convert it to `String`
-    let mut body: String = String::from_utf8(data).expect("body is not valid UTF8!");
-
-    let prefix: String = r#"{"ip":""#.to_string();
-    let mut suffix: String = String::new();
-    {
-        let re: Regex = Regex::new(r#"(","host\S+)"#).unwrap();
-        let Some(suffix_in_body) = re.captures(body.as_ref()) else {
-            panic!("There wasn't a ivp6 address in web-request present!");
-        };
-        let _ = &suffix_in_body[0].clone_into(&mut suffix);
-    }
-
-    //Remove prefix and suffix from returned body
-    body = body.strip_prefix(&prefix).unwrap().to_string();
-    body = body.strip_suffix(&suffix).unwrap().to_string();
-    body += &*"/".to_string();
-    body += &netmask.to_string();
-
-    return Ipv6Net::from_str(body.as_str()).unwrap();
-}
-
 fn read_config(config_path: &String) -> String {
     let mut file = std::fs::File::open(config_path).expect("Can't open file!");
     let mut contents = String::new();
